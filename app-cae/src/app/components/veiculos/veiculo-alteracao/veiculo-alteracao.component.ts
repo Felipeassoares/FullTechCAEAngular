3e723e758f3e86d1413c3fec56fe58d2c7b502b3
diurno <jessiannesaiara.js@gmail.com>
2023-08-31 14:41:08 -0300

    Validação da placa com inclusão da mensagem de veiculo já cadastrado e inclusão da vaga atual na alteração de veículo

--- app-cae/src/app/components/veiculos/veiculo-alteracao/veiculo-alteracao.component.ts
+++ app-cae/src/app/components/veiculos/veiculo-alteracao/veiculo-alteracao.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Vaga } from 'src/app/classes/vaga';
 import { Veiculo } from 'src/app/classes/veiculo';
 import { VeiculoService } from 'src/app/services/veiculo.service';
 
@@ -8,6 +9,7 @@ import { VeiculoService } from 'src/app/services/veiculo.service';
   templateUrl: './veiculo-alteracao.component.html',
   styleUrls: ['./veiculo-alteracao.component.css']
 })
+
 export class VeiculoAlteracaoComponent implements OnInit{
   constructor(
     private router: Router,
@@ -18,8 +20,11 @@ export class VeiculoAlteracaoComponent implements OnInit{
   placa!: string;
 
   ngOnInit(): void {
-     this.placa = this.route.snapshot.paramMap.get('id') as string;
+    this.placa = this.route.snapshot.paramMap.get('id') as string;
     this.buscar(this.placa);
+    this.carregarVagasDisponiveis();
+    this.vagaSelecionada = this.veiculo.idVaga;
+    
   }
 
   buscar(placa: string): void {
@@ -27,18 +32,45 @@ export class VeiculoAlteracaoComponent implements OnInit{
       .subscribe(resposta => this.veiculo = resposta);
   }
 
+  vagasDisponiveis: Vaga[] = [];
+  vagaAtual: Vaga[] = []; // propriedade para armazenar as vagas ocupadas
+  vagaSelecionada: number | undefined;
+
+  carregarVagasDisponiveis(): void {    
+    this.service.getVagasDisponiveis().subscribe(vagas => {
+      this.service.getVeiculosApi().subscribe(veiculos => {
+        const vagaAtual = veiculos.map(vc => vc.vaga);
+        this.vagasDisponiveis = vagas.filter(vg => !vagaAtual.includes(vg.id));
+        this.vagaAtual = vagas.filter(vg => vagaAtual.includes(vg.id));
+      });
+    });
+    // this.service.getVagasDisponiveis().subscribe(vagas => {
+    //   this.service.getVeiculosApi().subscribe(veiculos => {
+    //     this.vagasDisponiveis = vagas.filter(vg => {
+    //       return !veiculos.some(vc => vc.vaga === vg.id);
+    //     });
+    //   });
+    // });
+  }
+
+
+
   fechar() {
     this.router.navigate(['/veiculos']);
   }
 
   alterar(veiculo: Veiculo) {
+    if (this.vagaSelecionada !== undefined) {
+      veiculo.idVaga = this.vagaSelecionada;
+    }
+    
     this.service.putVeiculoApi(veiculo, this.placa)
       .subscribe({
         complete: () => this.fechar(),
-        error: erro => {
+       /* error: erro => {
           console.error(erro);
           window.alert(erro);
-        }
+        }*/
       });
   }
 
