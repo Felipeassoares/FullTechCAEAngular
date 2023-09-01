9e0e7df954971f4bc48a7d559f343c5660bd9ac5
Jessianne Macedo <jessiannesaiara.js@gmail.com>
2023-08-31 22:56:24 -0300

    path veiculo-lista

--- app-cae/src/app/modules/veiculos/veiculos-routing.module.ts
+++ app-cae/src/app/modules/veiculos/veiculos-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VeiculoAlteracaoComponent } from 'src/app/components/veiculos/veiculo-alteracao/veiculo-alteracao.component';
+import { VeiculoRemocaoComponent } from 'src/app/components/veiculos/veiculo-remocao/veiculo-remocao.component';
 import { VeiculosListaComponent } from 'src/app/components/veiculos/veiculos-lista/veiculos-lista.component';
 import { VeiculosNovoComponent } from 'src/app/components/veiculos/veiculos-novo/veiculos-novo.component';
 import { VeiculosComponent } from 'src/app/components/veiculos/veiculos/veiculos.component';
@@ -7,7 +9,9 @@ import { VeiculosComponent } from 'src/app/components/veiculos/veiculos/veiculos
 const routes: Routes = [
   { path: "", component: VeiculosComponent},
   { path: "novo", component: VeiculosNovoComponent},
-  { path: "lista", component: VeiculosListaComponent}
+  { path: "lista", component: VeiculosListaComponent},
+  {path:"alteracao/:id", component: VeiculoAlteracaoComponent},
+  {path: "remocao/:id", component: VeiculoRemocaoComponent}
 ];
 
 @NgModule({
